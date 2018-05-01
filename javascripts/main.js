e5d73f1d4716dbd56c38ee40e6deab511050240f
Nathan Pabst <nathan.pabst@gmail.com>
2018-04-30 21:26:00 -0500

    fish filter button done

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,24 @@
 // Filter fish that are "on sale"
+const filterFish = () => {
+  $('#available .fish').not('.on-sale').toggle();
+};
+
+const changeButtonText = () => {
+  $('#show-sale').text((i, text) => {
+    let returnText = '';
+    if (text === "Show Sale Fish") {
+      returnText = "Show All";
+    } else {
+      returnText = "Show Sale Fish";
+    }
+    return returnText;
+  });
+};
+
+$('#show-sale').click(() => {
+  changeButtonText();
+  filterFish();
+});
 
 // Add fish to "Basket"
 const moveToCart = (e) => {
