78ac459e7de73e34c0606167041f19266bb03b5c
Rita Patrick <rita.patrick1007@gmail.com>
2018-04-30 20:59:08 -0500

    move card to fish basket

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,10 @@
 // Filter fish that are "on sale"
 
 // Add fish to "Basket"
+const moveToCart = (e) => {
+  let fishCard = $(e.target).closest('.fish'); //stops at parent with class fish
+  // console.log(fishCard);
+  $('#snagged').append(fishCard);
+};
+
+$('button.add').click(moveToCart);
