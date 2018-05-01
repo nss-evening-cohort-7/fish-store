f7c42d02e368e54faa47d065662892824cb4778e
Anastasiia Somchyk <e4nastya@gmail.com>
2018-04-30 21:00:59 -0500

    adding fish to basket

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,10 @@
 // Filter fish that are "on sale"
 
 // Add fish to "Basket"
+const moveToCard = (e) => {
+  let fishCard = $(e.target).closest('.fish');
+  console.log('fishCard', fishCard);
+  $('#snugged').append('fishCard);
+}
+
+$('button.add').click(moveToCard);
