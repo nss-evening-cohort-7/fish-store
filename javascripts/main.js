1283277e8ffd498977d71b4a14567c75855774a2
Rita Patrick <rita.patrick1007@gmail.com>
2018-04-30 21:24:57 -0500

    added fish for sale

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,24 @@
 // Filter fish that are "on sale"
+const filterFish = () => {
+  $('#available .fish').not('.on-sale').toggle();
+};
 
+const changeButtonText = () => {
+  $('#show-sale').text((i, text) => {
+    let returnText = '';
+    if (text === "Show Sale Fish") {
+      returnText = "Show All";
+    } else {
+      returnText = "Show Sale Fish";
+    }
+    return returnText;
+  })
+};
+
+$('#show-sale').click(() => { //click(filterFish);
+  changeButtonText();
+  filterFish();
+});
 // Add fish to "Basket"
 const moveToCart = (e) => {
   let fishCard = $(e.target).closest('.fish'); //stops at parent with class fish
@@ -8,3 +27,5 @@ const moveToCart = (e) => {
 };
 
 $('button.add').click(moveToCart);
+
+
