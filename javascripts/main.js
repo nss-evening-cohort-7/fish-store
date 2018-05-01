db1b9cbdad53e96a381a883da8e5919d090d7c06
Nathan Pabst <nathan.pabst@gmail.com>
2018-04-30 21:01:06 -0500

    wip

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,10 @@
 // Filter fish that are "on sale"
 
 // Add fish to "Basket"
+const moveToCart = (e) => {
+  let fishCard = $(e.target).closest('.fish');
+  $('#snagged').append(fishCard);
+  // $('input').val();  
+};
+
+$('button.add').click(moveToCart);
