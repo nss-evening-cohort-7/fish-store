838de1eb9e99ce375a8403e25609ccecae3ae947
Callan Morrison <callan.l.morrison@gmail.com>
2018-05-01 18:36:21 -0500

    events attached

--- javascripts/events.js
+++ javascripts/events.js
@@ -15,15 +15,18 @@ const changeButtonText = () => {
   });
 };
 
-$('#show-sale').click(() => {
-  changeButtonText();
-  filterFish();
-});
-
 // Add fish to "Basket"
 const moveToCart = (e) => {
   const fishCard = $(e.target).closest('.fish');
   $('#snagged').append(fishCard);
 };
 
-$('button.add').click(moveToCart);
+const bindEvents = () => {
+  $('button.add').click(moveToCart);
+  $('#show-sale').click(() => {
+    changeButtonText();
+    filterFish();
+  });
+};
+
+module.exports = bindEvents;
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,3 @@
-console.log('hello from Main');
+const bindEvents = require('./events');
+
+bindEvents();
